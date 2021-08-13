1d7ad5907977865a22f291f07124aacf93e2af54
Tina Shan <78404774+bigleafenergy@users.noreply.github.com>
2021-08-13 12:07:00 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-blue-400 pt-64 text-center h-screen">
+      Hello
+    </div>
+  )
+}
+
+export default App
